81cf0a06a5be226a1d242b421435f98e76a7c5f1
Zach <zachary.goldstein@concurrent-rt.com>
2022-07-13 11:47:24 -0400

    Fixed install/uninstall scripts not exiting when user answers no

--- install.sh
+++ install.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
-#set -e
 
-function ynPrompt() {
+function yn_prompt() {
 	while true; do
 		read -p "$1" yn
 		case $yn in
-			[Yy]* ) break;;
-			[Nn]* ) return 0;;
-			* ) echo "Please answer yes or no.";;
+			[Yy]* ) return 0;;
+			[Nn]* ) return 1;;
+			* ) echo "Please answer y (yes) or n (no).";;
 		esac
 	done
 }
 
 SCRIPT_PATH=$(realpath $(dirname -- $BASH_SOURCE))
 
-if (! command ros2 &> /dev/null)
-then
+if (! command ros2 &> /dev/null); then
 	echo "Could not find ros2. Make sure to source ros2 before running this script."
 	return 1
 fi
@@ -23,30 +21,27 @@ fi
 ROS2_DIR=$(which ros2)
 
 # Gets the base directory of ros2
-for i in {0..3}
-do
-  ROS2_DIR=$(dirname $ROS2_DIR) &> /dev/null
+for i in {0..3}; do
+	ROS2_DIR=$(dirname $ROS2_DIR) &> /dev/null
 done
 
 # Makes sure the src directory exists inside ROS2_DIR
 # src directory will only exist when ros2 is built from source
-if [ ! -d "$ROS2_DIR/src" ]
-then
+if [ ! -d "$ROS2_DIR/src" ]; then
 	echo "Could not find the src directory inside $ROS2_DIR. Make sure ros is built from source."
 	return 1
 fi
 
-ynPrompt "Found ROS base directory at $ROS2_DIR. Continue with applying patch (y/n)?"
+yn_prompt "Found ROS base directory at $ROS2_DIR. Continue with applying patch (y/n)?" || return 0
 
 echo "Applying patch for rttest..."
 
 RTTEST_DIR="$ROS2_DIR/src/ros2/realtime_support"
 
 # Check if rttest is clean
-if [ ! -z "$(git -C $RTTEST_DIR status --porcelain)" ];
-then
-	echo "rttest is not clean"
-	ynPrompt "Clean rttest (y/n)?"
+if [ ! -z "$(git -C $RTTEST_DIR status --porcelain)" ]; then
+	echo "rttest has been previously modified. Continuing will reset all current changes to rttest."
+	yn_prompt "Continue (y/n)?" || return 0
 	git -C $RTTEST_DIR reset --hard > /dev/null
 fi
 
@@ -56,17 +51,16 @@ echo "Applying patch for pendulum demo..."
 
 DEMO_DIR="$ROS2_DIR/src/ros2/demos"
 
-# Check if rttest is clean
-if [ ! -z "$(git -C $DEMO_DIR status --porcelain)" ];
-then
-	echo "pendulum demo is not clean"
-	ynPrompt "Clean pendulum demo (y/n)?"
+# Check if pendulum_control ) is clean
+if [ ! -z "$(git -C $DEMO_DIR status --porcelain)" ]; then
+	echo "pendulum demo has been previously modified. Continuing will reset all current changes to pendulum demo."
+	yn_prompt "Continue (y/n)?" || return 0
 	git -C $DEMO_DIR reset --hard > /dev/null
 fi
 
 git -C $DEMO_DIR apply "$SCRIPT_PATH/demos_patch.patch"
 
-echo "All patches applied. Builing packages..."
+echo "All patches applied. Building packages..."
 
 cd $ROS2_DIR
 
--- uninstall.sh
+++ uninstall.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-function ynPrompt() {
+function yn_prompt() {
 	while true; do
 		read -p "$1" yn
 		case $yn in
-			[Yy]* ) break;;
-			[Nn]* ) return 0;;
+			[Yy]* ) return 0;;
+			[Nn]* ) return 1;;
 			* ) echo "Please answer yes or no.";;
 		esac
 	done
@@ -13,8 +13,7 @@ function ynPrompt() {
 
 SCRIPT_PATH=$(realpath $(dirname -- $BASH_SOURCE))
 
-if (! command ros2 &> /dev/null)
-then
+if (! command ros2 &> /dev/null); then
 	echo "Could not find ros2. Make sure to source ros2 before running this script."
 	return 1
 fi
@@ -22,20 +21,18 @@ fi
 ROS2_DIR=$(which ros2)
 
 # Gets the base directory of ros2
-for i in {0..3}
-do
-  ROS2_DIR=$(dirname $ROS2_DIR) &> /dev/null
+for i in {0..3}; do
+	ROS2_DIR=$(dirname $ROS2_DIR) &> /dev/null
 done
 
 # Makes sure the src directory exists inside ROS2_DIR
 # src directory will only exist when ros2 is built from source
-if [ ! -d "$ROS2_DIR/src" ]
-then
+if [ ! -d "$ROS2_DIR/src" ]; then
 	echo "Could not find the src directory inside $ROS2_DIR. Make sure ros is built from source."
 	return 1
 fi
 
-ynPrompt "Found ROS base directory at $ROS2_DIR. Continue with uninstallation (y/n)?"
+yn_prompt "Found ROS base directory at $ROS2_DIR. Continue with uninstallation (y/n)?" || return 0
 
 RTTEST_DIR="$ROS2_DIR/src/ros2/realtime_support"
 
@@ -52,4 +49,3 @@ colcon build --symlink-install --packages-select rttest pendulum_control pendulu
 cd - > /dev/null
 
 echo "Sucessfully uninstalled patches"
-
